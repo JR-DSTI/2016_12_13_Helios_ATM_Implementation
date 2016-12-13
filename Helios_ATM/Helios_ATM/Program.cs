c7c118f5d636c16dfef787b3e75415cd28801a7e
JR-DSTI <jonas.rathke@edu.dsti.institute>
2016-12-13 21:43:10 +0100

    Welcome window, Progressbar and first coding

--- Helios_ATM/Helios_ATM/Program.cs
+++ Helios_ATM/Helios_ATM/Program.cs
@@ -16,7 +16,7 @@ namespace Helios_ATM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ATM1());
         }
     }
 }
