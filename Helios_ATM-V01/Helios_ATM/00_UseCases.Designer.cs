82f351f5322295f8c46c490f76fbe1dda2a6d958
JR-DSTI <jonas.rathke@edu.dsti.institute>
2017-01-02 08:24:59 +0100

    creation of usecaseform

---
+++ Helios_ATM-V01/Helios_ATM/00_UseCases.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Helios_ATM
+{
+    partial class ATM0
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ATM1));
+            this.EnterHereButton = new MetroFramework.Controls.MetroButton();
+            this.WelcomeTimer = new System.Windows.Forms.Timer(this.components);
+            this.WelcomeProgressBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.BatteryCharge = new MetroFramework.Controls.MetroProgressBar();
+            this.NetworkSignal = new MetroFramework.Controls.MetroProgressSpinner();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // EnterHereButton
+            // 
+            this.EnterHereButton.Location = new System.Drawing.Point(164, 510);
+            this.EnterHereButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.EnterHereButton.Name = "EnterHereButton";
+            this.EnterHereButton.Size = new System.Drawing.Size(171, 23);
+            this.EnterHereButton.TabIndex = 0;
+            this.EnterHereButton.Text = "ENTER HERE";
+            this.EnterHereButton.UseSelectable = true;
+            this.EnterHereButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // WelcomeTimer
+            // 
+            this.WelcomeTimer.Interval = 1000;
+            this.WelcomeTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // WelcomeProgressBar
+            // 
+            this.WelcomeProgressBar.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.WelcomeProgressBar.Location = new System.Drawing.Point(140, 272);
+            this.WelcomeProgressBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.WelcomeProgressBar.Name = "WelcomeProgressBar";
+            this.WelcomeProgressBar.Size = new System.Drawing.Size(219, 16);
+            this.WelcomeProgressBar.TabIndex = 3;
+            this.WelcomeProgressBar.Visible = false;
+            this.WelcomeProgressBar.Click += new System.EventHandler(this.WelcomeProgressBar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(125, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 77);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "HELIOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 430);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(336, 77);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "WELCOME";
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(431, 452);
+            this.metroButton1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(61, 32);
+            this.metroButton1.TabIndex = 6;
+            this.metroButton1.Text = "FastTrack";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // BatteryCharge
+            // 
+            this.BatteryCharge.Location = new System.Drawing.Point(7, 15);
+            this.BatteryCharge.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BatteryCharge.Name = "BatteryCharge";
+            this.BatteryCharge.Size = new System.Drawing.Size(44, 17);
+            this.BatteryCharge.TabIndex = 8;
+            this.BatteryCharge.Value = 100;
+            this.BatteryCharge.Click += new System.EventHandler(this.BatteryCharge_Click);
+            // 
+            // NetworkSignal
+            // 
+            this.NetworkSignal.Location = new System.Drawing.Point(456, 15);
+            this.NetworkSignal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.NetworkSignal.Maximum = 100;
+            this.NetworkSignal.Name = "NetworkSignal";
+            this.NetworkSignal.Size = new System.Drawing.Size(45, 41);
+            this.NetworkSignal.TabIndex = 10;
+            this.NetworkSignal.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.NetworkSignal.UseSelectable = true;
+            this.NetworkSignal.Value = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(140, 250);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Initializing...";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(96, 357);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(125, 44);
+            this.metroButton2.TabIndex = 12;
+            this.metroButton2.Text = "Test";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // ATM1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(516, 544);
+            this.ControlBox = false;
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NetworkSignal);
+            this.Controls.Add(this.BatteryCharge);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.WelcomeProgressBar);
+            this.Controls.Add(this.EnterHereButton);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "ATM1";
+            this.Padding = new System.Windows.Forms.Padding(20, 74, 20, 20);
+            this.Load += new System.EventHandler(this.ATM0_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer WelcomeTimer;
+        private System.Windows.Forms.ProgressBar WelcomeProgressBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton EnterHereButton;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private MetroFramework.Controls.MetroProgressBar BatteryCharge;
+        private MetroFramework.Controls.MetroProgressSpinner NetworkSignal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private MetroFramework.Controls.MetroButton metroButton2;
+    }
+}
+
---
+++ Helios_ATM-V01/Helios_ATM/00_UseCases.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Windows;
+using System.IO;
+
+namespace Helios_ATM
+{
+   
+    public partial class ATM0 : MetroFramework.Forms.MetroForm
+    {
+        public ATM0()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.WelcomeProgressBar.Increment(10);  
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //timer to extend waiting and system setup :
+            this.WelcomeTimer.Start();
+            this.timer2.Start();
+            this.WelcomeProgressBar.Visible = true;
+
+            //Log current operation:
+            s3log.logOperation(sender);
+
+            //"await" needs the async in the function declaration and equals "Sleep"
+            await Task.Delay(2000);
+
+            //MetroFramework.MetroMessageBox.Show(this, (Pinger("stackoverflow.com", 4)).ToString());
+            if (Pinger("stackoverflow.com", 5) > 129)
+            {
+                MetroFramework.MetroMessageBox.Show(this, "the connection is lost"); // this = current form        
+                this.WelcomeProgressBar.Value = 0;
+                WelcomeTimer.Stop();
+                this.timer2.Stop();
+            }
+            else
+            {
+                //start the next form
+                Form ATM3 = new ATM3(); // Instantiate a Form object.
+                ATM3.Show(); //show the new Form
+                WelcomeTimer.Stop();
+
+                //hide the old form
+                this.Visible = false;  //Hide the old form
+            }
+
+        }
+        private void ATM0_Load(object sender, EventArgs e)
+        {
+            this.BatteryCharge.Value = battery.charge;
+            //battery.form1visit += 1;
+            //using (StreamWriter sr = new StreamWriter(@"Y:\Documents\GitHub\2016_12_13_Helios_ATM_Implementation\Project_Supplementary\2016_12_28_AWS_Log.txt"))
+            //{     
+            //    sr.WriteLine("Battery life: "+battery.charge +"% "+ (DateTime.Now).ToString());
+            //    sr.WriteLine("visit to Main Menu: "+ (battery.form1visit).ToString()+ (DateTime.Now).ToString());
+            //}
+        }
+        //Quick jump to current Form:
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+
+            if (Pinger("stackoverflow.com", 5) > 29)
+            {
+   
+                MetroFramework.MetroMessageBox.Show(this, "the connection is lost");
+            }
+            else
+            {
+
+            MetroFramework.Forms.MetroForm ATM6 = new ATM6(); // Instantiate a Form object.
+            ATM6.Show(); //show the new Form
+            this.timer2.Stop();
+
+            this.Visible = false;  //Hide the old form
+        }
+    }
+
+        private void WelcomeProgressBar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public double Pinger(string host, int echoNum)
+        {
+  
+        long totalTime = 0;
+        int timeOut = 120;
+        for (int i=0; i<echoNum; i++)
+            {
+                Ping pingSender=new Ping();
+                PingReply r = pingSender.Send(host,timeOut);
+                if (r.Status==IPStatus.Success)
+                {
+                    totalTime += r.RoundtripTime;
+                }
+            }
+            return (totalTime / echoNum);
+        }
+
+        private void BatteryCharge_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void discharge()
+        {
+            if (BatteryCharge.Value > 0)
+                {
+                BatteryCharge.Value -= battery.decrement;
+                battery.charge= BatteryCharge.Value;
+            }
+            else
+            {
+                AutoClosingMessageBox.Show("The ATM is running out of power. Disconnecting... and recharging.", "Low energy", 1500, this);
+                battery.charge = 100;
+                //Environment.Exit(0);
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.discharge();
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+
+}
